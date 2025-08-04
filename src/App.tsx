1e89d4b314ddb59346a33c62d065126c2ee8da2c
JZanataNeto <cartasdojoao@gmail.com>
2025-08-04 16:16:58 -0300

    criando componentes botao e formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
-  return <div className="App">Hello Word!</div>;
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
 }
 
 export default App;
