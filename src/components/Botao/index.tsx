a214569ce8f49b537e4481c9ea83f8469e92220f
JZanataNeto <cartasdojoao@gmail.com>
2025-08-18 15:48:55 -0300

    criando novos componentes

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import "./style.scss";
+import style from "./Botao.module.scss";
 
-class Botao extends React.Component {
+interface Props {
+  children: React.ReactNode;
+}
+class Botao extends React.Component<Props> {
   render() {
-    return <button className="botao">Botão</button>;
+    return <button className={style.botao}>{this.props.children}</button>;
   }
 }
 
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./style.scss";
+import style from "./Lista.module.scss";
+import Item from "./item";
 
 function Lista() {
   const tarefas = [
@@ -17,14 +18,11 @@ function Lista() {
     },
   ];
   return (
-    <aside className="listaTarefas">
+    <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className="item">
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item key={index} tarefa={item.tarefa} tempo={item.tempo} />
         ))}
       </ul>
     </aside>
